4c4de4fe42c5c43972059f9b185f65ec870c2dd7
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2019-10-30 11:00:58 -0300

    third attempt trying to fix proxy issues

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.APP_URL || 'http://localhost:3333/'
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333/'
+      : process.env.APP_URL
 });
 
 export default api;
