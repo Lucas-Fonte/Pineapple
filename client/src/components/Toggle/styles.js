2e92f65e6d83db30ca57579ac1affd428227d68c
Planejamento <planejamento@confetti.com.br>
2019-10-31 18:12:24 -0300

    drawer configured

--- client/src/components/Toggle/styles.js
+++ client/src/components/Toggle/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+export const ToggleButton = styled.div`
   position: fixed;
   top: 80vh;
   left: 80vw;
