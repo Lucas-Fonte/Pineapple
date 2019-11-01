c4ec160cb0a09f6451453bd099ecd1cdab710a4c
igorgonzalezgg <56846471+igorgonzalezgg@users.noreply.github.com>
2019-11-01 09:17:58 -0300

    icon test

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  username: 'postgres',
+  password: 'factorello',
+  database: 'factorello',
   define: {
     timestamps: true,
     underscored: true,
