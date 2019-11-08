2552d4bfef29b0bc509dfe7debcd10928c8a1499
Planejamento <planejamento@confetti.com.br>
2019-11-08 14:22:24 -0300

    another pwa test

--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     padding: 0 20px;
+
+    @media only screen and (max-width: 696px) {
+        padding-top: 20px;
+    }
 `;
 
 export const Content = styled.div`
